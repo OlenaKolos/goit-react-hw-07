a51930c0e99b426b3b4ac9bd065a06ef323c36d8
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-04-05 17:47:04 +0300

    add css

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -14,20 +14,3 @@ const filtersSlice = createSlice({
 
 export const { changeFilter } = filtersSlice.actions;
 export const filterReducer = filtersSlice.reducer;
-
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const filtersSlice = createSlice({
-//   name: "filters",
-//   initialState: {
-//     name: "",
-//   },
-//   reducers: {
-//     changeFilter(state, action) {
-//       state.name = action.payload;
-//     },
-//   },
-// });
-
-// export const { changeFilter } = filtersSlice.actions;
-// export const filterReducer = filtersSlice.reducer; // експортується filterReducer, а не filtersReducer
